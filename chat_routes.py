51dd1c8180d0dda8ed764f02762f36f08fed3454
Shivasai Chinthala <shivasaichinthala21@gmail.com>
2026-05-02 23:33:14 +0530

    Refactor chat event handlers for message history
    Removed connection and leave event handlers, added message history retrieval and message saving.

--- chat_routes.py
+++ chat_routes.py
@@ -1,24 +1,16 @@
-from flask_socketio import emit, join_room, leave_room
+from flask_socketio import emit, join_room
+from models import save_message, get_messages
 
 def register_chat_events(socketio):
-    @socketio.on('connect')
-    def handle_connect():
-        emit('message', {'data': 'Welcome to Realtime Chat!'})
-
     @socketio.on('join')
     def handle_join(data):
         room = data['room']
         join_room(room)
-        emit('message', {'data': f"Joined room {room}"}, room=room)
+        history = get_messages(room)
+        emit('history', {'messages': history}, room=request.sid)
 
     @socketio.on('chat')
     def handle_chat(data):
-        room = data['room']
-        msg = data['msg']
-        emit('chat', {'msg': msg}, room=room)
-
-    @socketio.on('leave')
-    def handle_leave(data):
-        room = data['room']
-        leave_room(room)
-        emit('message', {'data': f"Left room {room}"}, room=room)
+        room, user, msg = data['room'], data['user'], data['msg']
+        save_message(room, user, msg)
+        emit('chat', {'user': user, 'msg': msg}, room=room)
